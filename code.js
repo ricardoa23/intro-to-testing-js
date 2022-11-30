5c50138f54b3ff38d978650a5ceaa170dd0777a9
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2022-11-30 16:29:03 -0600

    added test and the ability to say "hello" to any input

--- code.js
+++ code.js
@@ -8,11 +8,5 @@ function helloWorld() {
 // }
 
 function sayHello(input) {
-    if (input === 'Alex') {
-        return `Hello, ${input}!`;
-    }else if (input === 'Pat') {
-        return `Hello, ${input}!`
-    }else {
-        return `Hello, ${input}!`
-    }
+    return `Hello, ${input}!`;
 }
