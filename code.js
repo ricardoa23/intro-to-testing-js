88c95fd4593e7619f5410999d68366eab7da31f1
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2022-12-05 19:29:49 -0600

    completed isEven and isVowel

--- code.js
+++ code.js
@@ -23,8 +23,27 @@ function sayHello(input) {
     return (typeof input === 'string') ? `Hello, ${input}!` : 'Hello, World!';
 }
 
-console.log(isFive('5'))
-
 function isFive(num) {
     return parseInt(num) === 5;
 }
+// console.log(isFive('5'))
+
+function isEven(num) {
+    return parseFloat(num) % 2 === 0;
+}
+// console.log(isEven(true))
+
+
+//isVowel
+function isVowel(input) {
+    if (typeof (input) === 'string') {
+        input = input.toLowerCase()
+        console.log(`prior conditional ${input}`);
+        let result = (input === 'a' || input === 'e' || input === 'i' || input === 'o' || input === 'u') ? true : false;
+        return result;
+    } else {
+        return false;
+    }
+}
+isVowel('A')
+console.log(isVowel('A'))
--- tests.js
+++ tests.js
@@ -48,7 +48,7 @@ describe('isFive', function() {
     it('should be a defined function', function () {
         expect(typeof isFive).toBe('function');
     });
-    it('should return a number when called', function () {
+    it('should return a boolean when called', function () {
         expect(typeof isFive()).toBe('boolean');
     });
     it('should return true when passed 5', function () {
@@ -68,3 +68,71 @@ describe('isFive', function() {
     });
 
 })
+
+//isEven
+
+let randomBoolean = Boolean(Math.round(Math.random()))
+
+describe('isEven', function () {
+    it('should be a defined function', function () {
+        expect(typeof (isEven)).toBe('function');
+    });
+    it('should  return a boolean when called', function () {
+        expect(typeof(isEven())).toBe('boolean');
+    });
+    it('should return true when passed 2', function () {
+        expect(isEven(2)).toBe(true);
+    })
+    it('should return a value of true when passed -4', function () {
+        expect(isEven(-4)).toBe(true)
+    });
+    it('should return false when passed 3', function () {
+        expect(isEven(3)).toBe(false);
+    });
+    it('should return false when passed "banana"', function () {
+        expect(isEven('banana')).toBe(false);
+    });
+    it('should be true when passed "8"', function () {
+        expect(isEven('8')).toBe(true);
+    });
+    it('should return false when passed Infinity', function () {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it(`should return false when passed a ${randomBoolean}`, function () {
+        expect(isEven(randomBoolean)).not.toBe(true);
+    });
+    it('should return false when passed without an argument', function () {
+        expect(isEven(undefined)).toBe(false);
+    });
+})
+
+describe('isVowel', function () {
+    it('should be a defined function', function () {
+        expect(typeof (isVowel)).toBe('function');
+    });
+    it('should return a boolean when called', function () {
+        expect(typeof(isVowel())).toBe('boolean');
+    });
+    it('should return a value of true when passed "a"', function () {
+        expect(isVowel('a')).toBe(true);
+    });
+    it('should return a value of true when passed "A"', function () {
+        expect(isVowel('A')).toBe(true);
+    });
+    it('should return false when passed "y"', function () {
+        expect(isVowel('y')).toBe(false);
+    });
+    it('should return a value of false when passed 4', function () {
+        expect(isVowel(4)).toBe(false);
+    });
+    it(`should return false when passed ${randomBoolean} `, function () {
+        expect(isVowel(randomBoolean)).toBe(false);
+    });
+    it('should return false when passed "banana"', function () {
+        expect(isVowel('banana')).toBe(false);
+    });
+    it('should return false when not passed an argument', function () {
+        expect(isVowel()).toBe(false)
+    });
+
+});
